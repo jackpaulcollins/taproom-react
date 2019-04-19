bed31e7bdadcac47733bc3d14bb51f215bf11961
Ryan McLean <mcleanrs@comcast.net>
2019-04-19 14:04:29 -0700

    fix header so links appear in header not under it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import '../scss/styles.scss';
 
 function Header(){
+  let styles = {
+    display: 'flex',
+    alignItems: 'center'
+  }
   return (
-    <div>
-      <h1 className='bg-grn header'>Albums</h1>
+    <div className='bg-grn header' style={styles}>
+      <h1>Albums</h1>
       <Link to="/">Home</Link> | <Link to="/newAlbum">Create Ticket</Link>
     </div>
   );
